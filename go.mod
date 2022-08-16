ffe90163a4b7e83e88fd51cf3919b90687fd6f76
Hiromitsu Iwata <hiromitsu7@gmail.com>
2022-08-17 01:22:47 +0900

    go-psの内容を一部コピー

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/hiromitsuiwata/go-utils
 
-go 1.18
-
-require github.com/mitchellh/go-ps v1.0.0 // indirect
+go 1.19
--- ps.go
+++ ps.go
@@ -3,20 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
-
-	"github.com/mitchellh/go-ps"
+	"time"
 )
 
 func main() {
-	processes, err := ps.Processes()
+	processes, err := Processes()
 	if err != nil {
 		log.Println("cannot get processes: " + err.Error())
 	}
 
 	fmt.Printf("%6s %6s %s\n", "PID", "PPID", "Executable")
 	for _, proc := range processes {
-		fmt.Printf("%6d %6d %s\n", proc.Pid(), proc.PPid(), proc.Executable())
+		fmt.Printf("%6d %6d %21s %s\n", proc.Pid(), proc.PPid(), proc.CreationTime().Format(time.RFC3339), proc.Executable())
 	}
 
 }
-
